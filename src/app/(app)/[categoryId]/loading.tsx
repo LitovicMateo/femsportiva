4c4ce1bfdcc0696d9da54e5581d0cde315aef01e
LitovicMateo <litovicm@gmail.com>
2025-10-14 16:35:52 +0200

    loading screen

--- src/app/(app)/[categoryId]/loading.tsx
+++ src/app/(app)/[categoryId]/loading.tsx
@@ -1,9 +1,13 @@
+import LogoMain from "@/components/Assets/LogoMain";
 import Image from "next/image";
 
-
 export default function CategoryLoading() {
-    // Or a custom loading skeleton component
-    return <div className="w-full h-svh flex justify-center items-center">
-       <Image src={"/logo.png"} height={50} width={50} alt="logo" className="aspect-square max-w-[50px]" />
+  // Or a custom loading skeleton component
+  return (
+    <div className="flex h-svh w-full items-center justify-center">
+      <div className="h-[128px] w-[128px]">
+        <LogoMain />
+      </div>
     </div>
-  }
+  );
+}
